8e7354477cc6f87e76d8d8d959d20b9cf7907380
Thomas Euverte <thomaseuverte@gmail.com>
2022-01-03 10:39:31 +0100

    Amélioration controllers/user (vérif email, hash mdp, partie LOGIN et GETACCOUNT) / Ajout d'un fichier .env

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,27 +1,84 @@
-const db = require('../models/index');
+const db = require("../models/index");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 exports.signup = (req, res, next) => {
-   
-    db.user.create({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        password: req.body.password,
-        isAdmin: false
+  db.user
+    .findOne({
+      where: { email: req.body.email },
     })
 
-    .then(function (newUser) {
-        return res.status(201).json({
-          userId: newUser.id,
-        });
+    .then((user) => {
+      if (user) {
+        return res.status(401).json({ error: "Email déjà utilisé!" });
+      }
+      bcrypt.hash(req.body.password, 5).then((hash) => {
+        db.user.create({
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          email: req.body.email,
+          password: hash,
+          isAdmin: false,
+        })
       })
-      .catch(function (err) {
-        return res.status(500).json({ error: "error" });
-      });
+        .then(() => res.status(201).json({ message: "Utilisateur créé" }))
+        .catch(error => res.status(400).json({ error }));
+      
+    })
 
+    .catch((error) => res.status(500).json({ error: "error" }));
 };
 
 exports.login = (req, res, next) => {
-   
+  db.user
+    .findOne({ where: { email: req.body.email } })
 
-};
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({ error: "Utilisateur non trouvé! " });
+      }
+
+      bcrypt.compare(req.body.password, user.password)
+      .then(valid => {
+          if (!valid) {
+              return res.status(401).json({ error: 'Mot de passe incorrect! ' });
+          }
+          res.status(200).json({
+              email: user.email,
+              lastName: user.lastName,
+              firstName: user.firstName,
+              userId: user.id,
+              token: jwt.sign(
+                  { userId: user.id },
+                  process.env.TOKEN,
+                  { expiresIn: '2h' }
+              )
+          });
+      })
+      .catch( error => res.status(500).json({ error }))
+
+    })
+
+     .catch( error => res.status(500).json({ error: "error" }));
+
+  };
+
+  exports.getAccount = (req, res, next) => {
+    
+    db.user
+      .findOne({ 
+        attributes: ['id'],
+        where: { id: req.params.id } })
+  
+      .then((user) => {
+
+        if (!user) {
+          return res.status(401).json({ error: "Une erreur s'est produite ici => utilisateur inexistant" });
+        }
+        
+        res.status(200).json(user);
+      })
+  
+       .catch( error => res.status(500).json({ error: "Une erreur s'est produite" }));
+  
+    };
